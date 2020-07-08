a433efb1dac9c9f15736ef90f3ecf6ea77d48298
marcodescripts <67992438+marcodescripts@users.noreply.github.com>
2020-07-07 23:14:17 -0500

    Add files via upload

---
+++ primerPrograma.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Este es mi primer script de python.
+"""
+
+#print es la función que se utiliza en python para imprimir
+
+print('Hola Marco')
+print('que tengas lindo día')
+
+apellido = input('Introduce tu apellido : ')
+
+print('Hola Marco '+apellido)
+
+
