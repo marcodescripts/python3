d5f2918e129f36bbe4d8aaaff12613aa1af1db37
marcodescripts <67992438+marcodescripts@users.noreply.github.com>
2020-07-07 23:27:07 -0500

    Update primerPrograma.py

--- primerPrograma.py
+++ primerPrograma.py
@@ -7,9 +7,12 @@ Este es mi primer script de python.
 
 print('Hola Marco')
 print('que tengas lindo día')
+# Declaramos la variable (apellido), para guardar un dato en este caso un apellido introducido por teclado.
 
+# la función (input) sirve para introducir datos a través del teclado.
 apellido = input('Introduce tu apellido : ')
 
+# en esta impresión estamos concatenando una cadena y una variables
 print('Hola Marco '+apellido)
 
 
