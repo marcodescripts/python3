5fde11972caa88f25f5371d7c661783cfacf35b9
marcodescripts <67992438+marcodescripts@users.noreply.github.com>
2020-08-17 11:14:24 -0500

    Add files via upload

---
+++ tiposdedatos.py
@@ -0,0 +1,55 @@
+
+numeroEntero = 28
+numeroFloat = 70.5
+
+cadena = 'aprendiendo python'
+
+print(numeroEntero)
+print(numeroFloat)
+print(cadena)
+
+
+# Operaciones aritméticas ( Al final del video cambiamos los voalores
+#  numeroUno y de numeroDos para ejemplificar mejor el módulo )
+
+numeroUno = 95
+numeroDos = 45
+
+# suma
+numeroTres = numeroUno + numeroDos
+print(numeroTres)
+
+# Resta
+numeroTres = numeroUno - numeroDos
+print(numeroTres)
+ 
+ # Multiplicación
+numeroTres = numeroUno * numeroDos
+print(numeroTres)
+
+# División
+numeroTres = numeroUno / numeroDos
+print(numeroTres)
+ 
+# Módulo
+numeroTres = numeroUno % numeroDos
+print(numeroTres)
+ 
+# Potencia
+numeroTres = numeroUno ** numeroDos
+print(numeroTres)
+
+
+
+cadena = 'aprendiendo python'
+numero = 23
+cadena2 = ' es divertido'
+#print(numero + cadena) # aquí dará error,son diferentes tipos de datos
+print(cadena + cadena2)
+
+
+
+
+
+
+
